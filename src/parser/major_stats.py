8ba2f9df3f74797fa529b3449ceb5fbe506c189f
Jason Xu <jasonxuuuu@gmail.com>
2024-12-30 15:36:17 -0800

    Created class to represent the stats for major

---
+++ src/parser/major_stats.py
@@ -0,0 +1,15 @@
+class MajorStats:
+    def __init__(self, name, id, type, year, space, max_grade, min_grade, initial_admit, final_admit):
+        self.year = self
+        self.name = name
+        self.id = id
+        self.type = type
+        self.year = year
+        self.space = space
+        self.max_grade = max_grade
+        self.min_grade = min_grade
+        self.initial_admit = initial_admit
+        self.final_admit = final_admit
+
+    def __str__(self):
+        print()
