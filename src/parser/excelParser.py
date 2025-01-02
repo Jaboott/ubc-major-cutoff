890605afa53a5d5393305258de7fc14db26820a4
Jason Xu <jasonxuuuu@gmail.com>
2025-01-01 16:43:43 -0800

    Implemented methods to parse type of major and improved error handling

--- src/parser/excelParser.py
+++ src/parser/excelParser.py
@@ -1,3 +1,5 @@
+import json
+
 from dotenv import load_dotenv
 
 import os
@@ -22,6 +24,19 @@ def read_document():
     return None
 
 
+def load_config():
+    path = "src/parser/config.json"
+
+    try:
+        with open(path, "r") as file:
+            config = json.load(file)
+            return config
+    except FileNotFoundError:
+        raise ValueError("Configuration file not found at 'src/parser/config.json'.")
+    except:
+        raise ValueError("Configuration file is not a valid JSON.")
+
+
 def get_major_id(data):
     if pd.isna(data):
         return None
@@ -31,7 +46,7 @@ def get_major_id(data):
     match = re.search(pattern, data)
 
     if match:
-        id = match.group(1)
+        id = match.group(1).strip()
         return id
     else:
         return None
@@ -44,7 +59,7 @@ def get_major_name(data):
     try:
         major_spec = data.split(": ")
         major_name = major_spec[1]
-        pattern = r'^(.*?)\s*(?:\((.*?)\))?$'
+        pattern = r'^(.*?)(?:\s*\(.*)?$'
     except:
         # regex to get the string containing the major name, also get the string that's surrounded by brackets
         pattern = r'^(?:Major|Combined Major|Honours|Combined Honours|)(?: \(\d+\))?[: ]?\s*([^()]+)'
@@ -59,16 +74,47 @@ def get_major_name(data):
         return None
 
 
+def get_major_type(data):
+    if pd.isna(data):
+        return None
+
+    pattern = r'\b(Major|Combined Major|Honours|Combined Honours)\b'
+    match = re.search(pattern, data)
+
+    if match:
+        major_type = match.group(1).strip()
+        return major_type
+    else:
+        return None
+
+
 def build_major_stats(data):
-    name = get_major_name(data["Spec"])
-    # name = None
-    id = get_major_id(data["Spec"])
-    type = None
-    year = data["Year"]
-    max_grade = data["Max grade"]
-    min_grade = data["Min Grade"]
-    initial_reject = data["Inital Reject \nNumber"]
-    final_admit = data["Final Admit \nNumber"]
+    # loading columns mapping and validating keys
+    try:
+        config = load_config()
+        columns = config.get("columns_mapping", {})
+
+        required_keys = ["name", "id", "type", "year", "max_grade", "min_grade", "initial_reject", "final_admit"]
+
+        for key in required_keys:
+            if key not in columns:
+                raise ValueError(f"Missing required keys in configuration: {key}")
+    except Exception as e:
+        print(e)
+        return None
+
+    name = get_major_name(data.iloc[columns["name"]])
+    id = get_major_id(data.iloc[columns["id"]])
+    type = get_major_type(data.iloc[columns["type"]])
+    year = data.iloc[columns["year"]]
+    max_grade = data.iloc[columns["max_grade"]]
+    min_grade = data.iloc[columns["min_grade"]]
+    initial_reject = data.iloc[columns["initial_reject"]]
+    final_admit = data.iloc[columns["final_admit"]]
+
+    # indicating empty row if major_name is missing
+    if name is None:
+        return None
 
     major_stats = MajorStats(name, id, type, year, max_grade, min_grade, initial_reject, final_admit)
 
@@ -79,6 +125,10 @@ if __name__ == '__main__':
     df = read_document()
     print(df.head())
 
+    data = []
+
     for i in range(df.shape[0]):
         major_stats = build_major_stats(df.iloc[i])
-        print(major_stats)
+        if major_stats is not None:
+            print(major_stats)
+            data.append(major_stats)
