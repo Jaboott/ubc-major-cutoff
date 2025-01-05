8dae7ebebc5d88e5fa4a2abbd82fb8918648793d
Jason Xu <jasonxuuuu@gmail.com>
2025-01-05 00:21:41 -0800

    Changed schema to be more specific, added additional table to record the checksum.

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,17 +1,28 @@
-CREATE TABLE AdmissionStatus (
-    Year INT,
-    MaxGrade DOUBLE,
-    MinGrade DOUBLE,
-    InitialReject INT,
-    FinalAdmit INT,
-    ID INT,
-    FOREIGN KEY (ID) REFERENCES Major(ID),
-    PRIMARY KEY (Year, ID)
+-- uncomment for initial db setup --
+-- CREATE TYPE major_type AS ENUM('Combined', 'Honours', 'Major');
+
+CREATE TABLE IF NOT EXISTS majors (
+    name VARCHAR(255),
+    id INT NOT NULL,
+    type  major_type NOT NULL,
+    PRIMARY KEY (id, type)
 );
 
-CREATE TABLE Major (
-    Name VARCHAR(255),
-    ID INT NOT NULL,
-    type ENUM('Combined', 'Honours', 'Major') NOT NULL,
-    PRIMARY KEY (ID)
+CREATE TABLE IF NOT EXISTS admission_statistics (
+    year INT,
+    max_grade NUMERIC,
+    min_grade NUMERIC,
+    initial_reject INT,
+    final_admit INT,
+    id INT,
+    type major_type NOT NULL,
+    FOREIGN KEY (id, type) REFERENCES majors(id, type),
+    PRIMARY KEY (year, id, type)
+);
+
+CREATE TABLE IF NOT EXISTS meta_data (
+    id SERIAL,
+    check_sum VARCHAR(64),
+    last_updated TIMESTAMP,
+    PRIMARY KEY (id)
 );
