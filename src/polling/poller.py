a7bee58522f85d4008d1e00ed49c541d4f99d25a
Jason Xu <jasonxuuuu@gmail.com>
2025-01-05 00:22:21 -0800

    Changed the poller.py to handle db interactions instead of local arrays.

--- src/polling/poller.py
+++ src/polling/poller.py
@@ -1,12 +1,16 @@
+from datetime import datetime
+
 from dotenv import load_dotenv
 
 import os
 import pandas as pd
 import hashlib
 import time
+import logging
 
-# Replace with redis server later if implemented
-fake_db = {}
+from src.db.connection import get_connection, execute_query
+from src.parser.excelParser import build_major_stats
+
 load_dotenv()
 
 
@@ -18,52 +22,80 @@ def read_document():
         df = pd.read_csv(url)
         return df
     except FileNotFoundError:
-        print('File not found')
+        logging.error('File not found')
 
     return None
 
 
-def create_checksums(data):
+def create_checksum(data):
     return hashlib.sha256(data.to_string().encode('utf-8')).hexdigest()
 
 
-def init_checksums(data):
+def has_checksum_changed(data):
     if data is None:
         return None
 
-    fake_db["document_checksum"] = create_checksums(data)
-    fake_db["rows_checksum"] = data.apply(lambda row: create_checksums(row), axis=1).tolist()
+    try:
+        result = execute_query("SELECT COUNT(*) FROM meta_data;")
+        checksum = create_checksum(data)
+
+        # initial setup of checksum
+        if result[0][0] == 0:
+            dt = datetime.now()
+            execute_query("INSERT INTO meta_data (check_sum, last_updated) VALUES(%s, %s);", (checksum, dt))
+            return True
+
+        old_checksum = execute_query("SELECT check_sum FROM meta_data ORDER BY last_updated DESC LIMIT 1;")
+        if old_checksum[0][0] != checksum:
+            return True
+
+    except Exception as e:
+        logging.error(e)
+
+    return False
 
 
-# TODO does not update data
-# TODO use intersect or difference_update
-def verify_checksums(data):
-    if data is None:
-        return None
+# TODO complete the db stuff
+def handle_change(data):
+    new_checksum = create_checksum(data)
 
-    # document's checksum have been changed
-    if create_checksums(data) != fake_db["document_checksum"]:
-        # checksum of newly fetched data
-        rows_checksum = set(data.apply(lambda row: create_checksums(row), axis=1))
-        changed_data = rows_checksum.difference(set(fake_db["rows_checksum"]))
-        removed_data = set(fake_db["rows_checksum"]).difference(rows_checksum)
-        print(f"Changed data: {changed_data}, Removed data: {removed_data}")
+    # re-populating the db with the new data
+    for i in range(data.shape[0]):
+        major_stats = build_major_stats(data.iloc[i])
+        if major_stats is not None:
+            print(major_stats)
+
+    return
 
 
-def main():
+def start_db():
+    try:
+        schema_path = "src/db/schema.sql"
+        if not os.path.exists(schema_path):
+            raise FileNotFoundError(f"Schema file not found at {schema_path}")
+
+        with open(schema_path, "r") as file:
+            init_tables = file.read()
+            execute_query(init_tables)
+    except Exception as e:
+        logging.error(e)
+
+
+# TODO
+def poll():
     data = read_document()
-    # init the fake_db if first time running
-    if not fake_db:
-        init_checksums(data)
-    else:
-        verify_checksums(data)
-    # print(fake_db)
+
+    start_db()
+    print("db started")
+    if has_checksum_changed(data):
+        print("checksum changed")
+        handle_change(data)
 
 
 if __name__ == '__main__':
     # Computing time
     while True:
         start_time = time.time()
-        main()
+        poll()
         print("--- %s seconds ---" % (time.time() - start_time))
         time.sleep(5)
