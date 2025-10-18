0331d840c04bb47ae96605f4f9398817d7a9c021
Jason Xu <jasonxuuuu@gmail.com>
2025-10-17 23:26:21 -0700

    added /api/majors to route

--- backend/app.py
+++ backend/app.py
@@ -1,109 +1,143 @@
 import os
 
+from database import PostgresHandler
 from flask import Flask, jsonify
 from flask.cli import load_dotenv
-
-from database import PostgresHandler
+from flask_cors import CORS
 
 load_dotenv()
 app = Flask(__name__)
+CORS(
+    app,
+    supports_credentials=True,
+    origins=[
+        "http://localhost:3000",
+    ],
+)
 
 
 def create_db_connection():
     return PostgresHandler(
-        host=os.getenv('POSTGRES_HOST'),
-        user=os.getenv('POSTGRES_USER'),
-        password=os.getenv('POSTGRES_PASSWORD'),
-        database=os.getenv('POSTGRES_DB'),
+        host=os.getenv("POSTGRES_HOST"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        database=os.getenv("POSTGRES_DB"),
         port=5432,
     )
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return "Hello World!"
 
 
-@app.route('/api/ping')
+@app.route("/api/ping")
 def ping_db():
     db = create_db_connection()
     try:
-        db.execute('SELECT 1')
-        return jsonify({'status': 'ok', 'message': 'db connection successful'}), 200
+        db.execute("SELECT 1")
+        return jsonify({"status": "ok", "message": "db connection successful"}), 200
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+        return jsonify({"status": "error", "message": str(e)}), 500
     finally:
         db.close()
 
 
-@app.route('/api/major/<int:major_uid>')
+@app.route("/api/majors")
+def get_majors():
+    db = create_db_connection()
+    try:
+        majors = db.fetchall("select name, uid from majors")
+        if majors is None:
+            return jsonify({"status": "ok", "message": "failed to get majors"})
+        return jsonify({"status": "ok", "data": majors}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+    finally:
+        db.close()
+
+
+@app.route("/api/major/<int:major_uid>")
 def get_major(major_uid):
     db = create_db_connection()
     try:
         major_data = db.fetchone("select * from majors where uid = %s", (major_uid,))
         if major_data is None:
-            return jsonify({'status': 'error', 'message': 'major not found'}), 404
-        return jsonify({'status': 'ok', 'data': major_data}), 200
+            return jsonify({"status": "error", "message": "major not found"}), 404
+        return jsonify({"status": "ok", "data": major_data}), 200
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+        return jsonify({"status": "error", "message": str(e)}), 500
     finally:
         db.close()
 
 
-@app.route('/api/admission/<int:major_uid>')
+@app.route("/api/admission/<int:major_uid>")
 def get_admission_statistics(major_uid):
     db = create_db_connection()
     try:
-        admission_statistics = db.fetchall("select * from admission_statistics where uid = %s", (major_uid,))
+        admission_statistics = db.fetchall(
+            "select * from admission_statistics where uid = %s", (major_uid,)
+        )
         if admission_statistics is None:
-            return jsonify({'status': 'error', 'message': 'admission statistics not found'}), 404
-        return jsonify({'status': 'ok', 'data': admission_statistics}), 200
+            return (
+                jsonify(
+                    {"status": "error", "message": "admission statistics not found"}
+                ),
+                404,
+            )
+        return jsonify({"status": "ok", "data": admission_statistics}), 200
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+        return jsonify({"status": "error", "message": str(e)}), 500
     finally:
         db.close()
 
 
-@app.route('/api/average-cutoffs')
+@app.route("/api/average-cutoffs")
 def get_average_cutoff():
     db = create_db_connection()
     try:
         average_admission_statistics = db.fetchall(
             """
-            select year, avg(min_grade) as average_cutoff, avg(max_grade) as average_max_grade
+            select year, avg(min_grade) as cutoff, avg(max_grade) as average_max_grade, sum(initial_reject) as initial_rejects, sum(final_admit) as final_admits
             from admission_statistics
             group by year
-            order by year desc
             """
         )
         if average_admission_statistics is None:
-            return jsonify({'status': 'error', 'message': 'average cutoff not available'}), 404
-        return jsonify({'status': 'ok', 'data': average_admission_statistics}), 200
+            return (
+                jsonify({"status": "error", "message": "average cutoff not available"}),
+                404,
+            )
+        return jsonify({"status": "ok", "data": average_admission_statistics}), 200
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+        return jsonify({"status": "error", "message": str(e)}), 500
     finally:
         db.close()
 
-@app.route('/api/max-admissions')
+
+@app.route("/api/max-admissions")
 def get_max_cutoff():
     db = create_db_connection()
     try:
         max_admission_statistics = db.fetchall(
             """
-            select distinct on (year) year, uid, min_grade, max_grade
+            select distinct on (year) year, uid, min_grade as cutoff, max_grade
             from admission_statistics
             where min_grade is not null
             order by year, min_grade desc;
             """
         )
         if max_admission_statistics is None:
-            return jsonify({'status': 'error', 'message': 'max admissions not available'}), 404
-        return jsonify({'status': 'ok', 'data': max_admission_statistics}), 200
+            return (
+                jsonify({"status": "error", "message": "max admissions not available"}),
+                404,
+            )
+        return jsonify({"status": "ok", "data": max_admission_statistics}), 200
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+        return jsonify({"status": "error", "message": str(e)}), 500
     finally:
         db.close()
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
